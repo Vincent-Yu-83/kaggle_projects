c35e808d2c5952df4ac6b17b5ba97dde837dd89f
Vincent-Yu-83 <Vincent@wen1tech.com>
2024-07-27 20:23:39 +0800

    123

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-123
+from transformers import AutoTokenizer, AutoModel, utils
+from bertviz import model_view
+utils.logging.set_verbosity_error()  # Suppress standard warnings
+
+
+import os
+ 
+# 检查是否设置了http_proxy环境变量
+if 'https_proxy' in os.environ:
+    proxy_url = os.environ['https_proxy']
+    print(f"HTTP代理已设置: {proxy_url}")
+else:
+    print("HTTP代理未设置。")
+    
+    
+model_name = "microsoft/xtremedistil-l12-h384-uncased"  # Find popular HuggingFace models here: https://huggingface.co/models
+input_text = "The cat sat on the mat"  
+model = AutoModel.from_pretrained(model_name, output_attentions=True)  # Configure model to return attention values
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+inputs = tokenizer.encode(input_text, return_tensors='pt')  # Tokenize input text
+outputs = model(inputs)  # Run model
+attention = outputs[-1]  # Retrieve attention from model outputs
+tokens = tokenizer.convert_ids_to_tokens(inputs[0])  # Convert input ids to token strings
+model_view(attention, tokens)  # Display model view
